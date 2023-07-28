4d1a6660eb4b4a4fee48a7a401e3c21f3423e3af
Harsh Agrawal <ha0744106@gmail.com>
2023-07-28 22:17:19 +0530

    student_classes.py

---
+++ student_classes.py
@@ -0,0 +1,220 @@
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+menu_markup_attendence_class = InlineKeyboardMarkup()
+class_prep_button=InlineKeyboardButton("Class Prep", callback_data="class_prep")
+class_1_button=InlineKeyboardButton("Class 1", callback_data="class_1")
+class_2_button=InlineKeyboardButton("Class 2", callback_data="class_2")
+class_3_button=InlineKeyboardButton("Class 3", callback_data="class_3")
+class_4_button=InlineKeyboardButton("Class 4", callback_data="class_4")
+class_5_button=InlineKeyboardButton("Class 5", callback_data="class_5")
+class_6_button=InlineKeyboardButton("Class 6", callback_data="class_6")
+class_7_button=InlineKeyboardButton("Class 7", callback_data="class_7")
+class_8_button=InlineKeyboardButton("Class 8", callback_data="class_8")
+class_9_button=InlineKeyboardButton("Class 9", callback_data="class_9")
+class_10_button=InlineKeyboardButton("Class 10", callback_data="class_10")
+class_11_button=InlineKeyboardButton("Class 11", callback_data="class_11")
+class_12_button=InlineKeyboardButton("Class 12", callback_data="class_12")
+exit_button=InlineKeyboardButton("Exit", callback_data="exit")
+menu_markup_attendence_class.row(class_prep_button, class_1_button)
+menu_markup_attendence_class.row(class_2_button, class_3_button)
+menu_markup_attendence_class.row(class_4_button, class_5_button)
+menu_markup_attendence_class.row(class_6_button, class_7_button)
+menu_markup_attendence_class.row(class_8_button, class_9_button)
+menu_markup_attendence_class.row(class_10_button, class_11_button)
+menu_markup_attendence_class.row(class_12_button, exit_button)
+
+
+menu_markup_class_1 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=)#url of google sheet 
+section_b = InlineKeyboardButton("Section B", url=)#url of google sheet 
+section_c = InlineKeyboardButton("Section C", url=)#url of google sheet 
+section_d = InlineKeyboardButton("Section D", url=)#url of google sheet 
+section_e = InlineKeyboardButton("Section E", url=)#url of google sheet 
+menu_markup_class_1.row(section_a)
+menu_markup_class_1.row(section_b)
+menu_markup_class_1.row(section_c)
+menu_markup_class_1.row(section_d)
+menu_markup_class_1.row(section_e)
+
+menu_markup_class_2 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=)#url of google sheet 
+section_b = InlineKeyboardButton("Section B", url=)#url of google sheet 
+section_c = InlineKeyboardButton("Section C", url=)#url of google sheet 
+section_d = InlineKeyboardButton("Section D", url=)#url of google sheet 
+section_e = InlineKeyboardButton("Section E", url=)#url of google sheet 
+menu_markup_class_2.row(section_a)
+menu_markup_class_2.row(section_b)
+menu_markup_class_2.row(section_c)
+menu_markup_class_2.row(section_d)
+menu_markup_class_2.row(section_e)
+
+menu_markup_class_3 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=)#url of google sheet 
+section_b = InlineKeyboardButton("Section B", url=)#url of google sheet 
+section_c = InlineKeyboardButton("Section C", url=)#url of google sheet 
+section_d = InlineKeyboardButton("Section D", url=)#url of google sheet 
+section_e = InlineKeyboardButton("Section E", url=)#url of google sheet 
+section_f = InlineKeyboardButton("Section F", url=)#url of google sheet 
+menu_markup_class_3.row(section_a)
+menu_markup_class_3.row(section_b)
+menu_markup_class_3.row(section_c)
+menu_markup_class_3.row(section_d)
+menu_markup_class_3.row(section_e)
+menu_markup_class_3.row(section_f)
+
+menu_markup_class_4 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=)#url of google sheet 
+section_b = InlineKeyboardButton("Section B", url=)#url of google sheet 
+section_c = InlineKeyboardButton("Section C", url=)#url of google sheet 
+section_d = InlineKeyboardButton("Section D", url=)#url of google sheet 
+section_e = InlineKeyboardButton("Section E", url=)#url of google sheet 
+section_f = InlineKeyboardButton("Section F", url=)#url of google sheet 
+menu_markup_class_4.row(section_a)
+menu_markup_class_4.row(section_b)
+menu_markup_class_4.row(section_c)
+menu_markup_class_4.row(section_d)
+menu_markup_class_4.row(section_e)
+menu_markup_class_4.row(section_f)
+
+menu_markup_class_5 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=) #url of google sheet
+section_b = InlineKeyboardButton("Section B", url=) #url of google sheet
+section_c = InlineKeyboardButton("Section C", url=) #url of google sheet
+section_d = InlineKeyboardButton("Section D", url=) #url of google sheet
+section_e = InlineKeyboardButton("Section E", url=) #url of google sheet
+section_f = InlineKeyboardButton("Section F", url=) #url of google sheet
+menu_markup_class_5.row(section_a)
+menu_markup_class_5.row(section_b)
+menu_markup_class_5.row(section_c)
+menu_markup_class_5.row(section_d)
+menu_markup_class_5.row(section_e)
+menu_markup_class_5.row(section_f)
+
+menu_markup_class_6 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=) #url of google sheet
+section_b = InlineKeyboardButton("Section B", url=) #url of google sheet
+section_c = InlineKeyboardButton("Section C", url=) #url of google sheet
+section_d = InlineKeyboardButton("Section D", url=) #url of google sheet
+section_e = InlineKeyboardButton("Section E", url=) #url of google sheet
+section_f = InlineKeyboardButton("Section F", url=) #url of google sheet
+section_g = InlineKeyboardButton("Section G", url=) #url of google sheet
+menu_markup_class_6.row(section_a)
+menu_markup_class_6.row(section_b)
+menu_markup_class_6.row(section_c)
+menu_markup_class_6.row(section_d)
+menu_markup_class_6.row(section_e)
+menu_markup_class_6.row(section_f)
+menu_markup_class_6.row(section_g)
+
+menu_markup_class_7 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=) #url of google sheet
+section_b = InlineKeyboardButton("Section B", url=) #url of google sheet
+section_c = InlineKeyboardButton("Section C", url=) #url of google sheet
+section_d = InlineKeyboardButton("Section D", url=) #url of google sheet
+section_e = InlineKeyboardButton("Section E", url=) #url of google sheet
+section_f = InlineKeyboardButton("Section F", url=) #url of google sheet
+menu_markup_class_7.row(section_a)
+menu_markup_class_7.row(section_b)
+menu_markup_class_7.row(section_c)
+menu_markup_class_7.row(section_d)
+menu_markup_class_7.row(section_e)
+menu_markup_class_7.row(section_f)
+
+menu_markup_class_8 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=) #url of google sheet
+section_b = InlineKeyboardButton("Section B", url=) #url of google sheet
+section_c = InlineKeyboardButton("Section C", url=) #url of google sheet
+section_d = InlineKeyboardButton("Section D", url=) #url of google sheet
+section_e = InlineKeyboardButton("Section E", url=) #url of google sheet
+section_f = InlineKeyboardButton("Section F", url=) #url of google sheet
+menu_markup_class_8.row(section_a)
+menu_markup_class_8.row(section_b)
+menu_markup_class_8.row(section_c)
+menu_markup_class_8.row(section_d)
+menu_markup_class_8.row(section_e)
+menu_markup_class_8.row(section_f)
+
+menu_markup_class_9 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=) #url of google sheet
+section_b = InlineKeyboardButton("Section B", url=) #url of google sheet
+section_c = InlineKeyboardButton("Section C", url=) #url of google sheet
+section_d = InlineKeyboardButton("Section D", url=) #url of google sheet
+section_e = InlineKeyboardButton("Section E", url=) #url of google sheet
+menu_markup_class_9.row(section_a)
+menu_markup_class_9.row(section_b)
+menu_markup_class_9.row(section_c)
+menu_markup_class_9.row(section_d)
+menu_markup_class_9.row(section_e)
+
+menu_markup_class_10 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=)#url of google sheet 
+section_b = InlineKeyboardButton("Section B", url=)#url of google sheet 
+section_c = InlineKeyboardButton("Section C", url=)#url of google sheet 
+section_d = InlineKeyboardButton("Section D", url=)#url of google sheet 
+menu_markup_class_10.row(section_a)
+menu_markup_class_10.row(section_b)
+menu_markup_class_10.row(section_c)
+menu_markup_class_10.row(section_d)
+
+menu_markup_class_11 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=)#url of google sheet 
+section_b = InlineKeyboardButton("Section B", url=)#url of google sheet 
+section_c = InlineKeyboardButton("Section C", url=)#url of google sheet 
+menu_markup_class_11.row(section_a)
+menu_markup_class_11.row(section_b)
+menu_markup_class_11.row(section_c)
+
+menu_markup_class_12 = InlineKeyboardMarkup()
+section_a = InlineKeyboardButton("Section A", url=)#url of google sheet 
+section_b = InlineKeyboardButton("Section B", url=)#url of google sheet 
+section_c = InlineKeyboardButton("Section C", url=)#url of google sheet 
+menu_markup_class_12.row(section_a)
+menu_markup_class_12.row(section_b)
+menu_markup_class_12.row(section_c)
+
+
+def is_class(bot, data, chat_id):
+    if data == "student_attendence":
+        bot.send_message(chat_id, "Welcome to student portal")
+        bot.send_message(chat_id, "Enter Class:", reply_markup=menu_markup_attendence_class)
+
+    elif data=="class_prep":
+        bot.send_message(chat_id, "Under Update")
+
+    elif data=="class_1":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_1)
+
+    elif data=="class_2":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_2)
+
+    elif data=="class_3":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_3)
+
+    elif data=="class_4":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_4)
+
+    elif data=="class_5":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_5)
+
+    elif data=="class_6":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_6)
+
+    elif data=="class_7":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_7)
+
+    elif data=="class_8":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_8)
+
+    elif data=="class_9":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_9)
+
+    elif data=="class_10":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_10)
+
+    elif data=="class_11":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_11)
+
+    elif data=="class_12":
+        bot.send_message(chat_id, "Enter Section:", reply_markup=menu_markup_class_12)
+
